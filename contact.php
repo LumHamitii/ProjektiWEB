cb56945ec33414fbbb806dc34b09be7f23e6c9f3
LumHamitii <120671590+LumHamitii@users.noreply.github.com>
2023-02-19 15:12:54 +0100

    fix of issue
    Problem me klasat fas fa, ne css

--- contact.php
+++ contact.php
@@ -7,42 +7,69 @@
     <title>Timberland-KS</title>
 </head>
 <body>
-  
+    <header class="header">
+
+        <a href="#"> <img src="images/timberland.png" width="150" height="90"></a>   
+        <div class="navv">
+        <nav class="navbar">
+         <a href="index.php">HOME</a>
+         <a href="products.php">PRODUCTS</a>
+         <a href="aboutus.php"> ABOUT US</a>
+         <a href="findus.php">FIND US</a>
+         <a href="contact.php">CONTACT</a>
+        </nav>
+        </div>
+        <!-- <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <div class="fas fa-shopping-cart" id="cart-btn"></div>
+            <a href="login.php">
+                <div class="fas fa-user"></div>
+            </a>
+        </div>
+    
+        <form action="" class="search-form" >
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label> -->
+    </header>
     <br><br><br>
     <section class="footer" id="contact">
-            
-            <div class="container2">
-                <form>
-                    <h1 class="heading1"><span>Contact us</span></h1>
-                  <i class="fas fa-paper-plane"></i>
-            
-                  <div class="input-group">
-                    <label>Full Name</label>
-                    <input type="text" placeholder="Enter your name" id="contact-name" onkeyup="validateName()">
-                    <span id="name-error"></span>
-                  </div>
-            
-                  <div class="input-group">
-                    <label>Phone No.</label>
-                    <input type="tel" placeholder="123 456 789"id="contact-phone" onkeyup="validatePhone()">
-                    <span id="phone-error"></span>
-                  </div>
-            
-                  <div class="input-group">
-                    <label>Email Id</label>
-                    <input type="email" placeholder="Enter Email" id="contact-email" onkeyup="validateEmail()">
-                    <span id="email-error"></span>
-                  </div>   
-            
-                  <div class="input-group">
-                    <label>Your Message</label>
-                    <textarea rows="5" placeholder="Enter your message" id="contact-message" onkeyup="validateMessage()"></textarea>
-                    <span id="message-error"></span>
-                  </div>
-            
-                <button onclick=" return validateForm()">Submit</button>
-               <span id="submit-error"></span>
-              </form>
+
+        <div class="container2">
+            <form action="connect.php" method="post" >
+                <h1 class="heading1"><span>Contact us</span></h1>
+              <i class="fas fa-paper-plane"></i>
+    
+              <div class="input-group">
+                <label>Full Name</label>
+                <input type="text" placeholder="Enter your name" id="contact-name" onkeyup="validateName()" name="name">
+                <span id="name-error"></span>
+              </div>
+    
+              <div class="input-group">
+                <label>Phone No.</label>
+                <input type="tel" placeholder="123 456 789"id="contact-phone" onkeyup="validatePhone()" name="phone" >
+                <span id="phone-error"></span>
+              </div>
+    
+              <div class="input-group">
+                <label>Email Id</label>
+                <input type="email" placeholder="Enter Email" id="contact-email" onkeyup="validateEmail()" name="email">
+                <span id="email-error"></span>
+              </div>   
+    
+              <div class="input-group">
+                <label>Your Message</label>
+                <textarea rows="5" placeholder="Enter your message" id="contact-message" onkeyup="validateMessage()" name="msg"></textarea>
+                <span id="message-error"></span>
+              </div>
+    
+              <button onclick="validateForm()">Submit</button>
+           <span id="submit-error"></span>
+          </form>
+    
+          </div>
+    
+    </section>
   <footer>
     <div class="col">
       
